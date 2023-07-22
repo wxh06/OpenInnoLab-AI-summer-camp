4d104fc5549ce48f44767c29df076ae818b64d04
汪心禾 <wangxinhe06@gmail.com>
2023-07-22 20:26:49 +0800

    `os.listdir` -> `tf.data.Dataset.list_files`

--- baduanjin/predict.py
+++ baduanjin/predict.py
@@ -12,11 +12,10 @@ DIR = os.path.dirname(__file__)
 
 model = tf.keras.models.load_model(os.path.join(DIR, MODEL_PATH))
 
-files = [
-    os.path.join(DIR, "test_data", f)
-    for f in sorted(os.listdir(os.path.join(DIR, "test_data")))
-]
-images = tf.data.Dataset.from_tensor_slices(files).map(tf.io.read_file)
+files = tf.data.Dataset.list_files(
+    os.path.join(DIR, "test_data", "*.jpg"), shuffle=False
+)
+images = files.map(tf.io.read_file)
 images = images.map(tf.image.decode_jpeg)
 images = images.map(
     lambda image: tf.cast(tf.image.resize(image, (HEIGHT, WIDTH)), tf.float32) / 255.0
@@ -25,7 +24,7 @@ result = tf.argmax(model.predict(images), axis=1)
 
 df = pd.DataFrame(
     result,
-    index=[os.path.split(pathname)[1] for pathname in files],
+    index=[os.path.split(pathname.numpy().decode())[1] for pathname in files],
     columns=["prediction"],
 )
 df["pre_class"] = df.apply(
